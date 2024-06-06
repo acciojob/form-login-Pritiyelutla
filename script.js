165c598eede33a0adb948a2625291f3200ab1580
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-06 22:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
+	let form = document.getElementById("form1");
     //Write your code here
+	let fname = document.getElementById("fname");
+	let lname = document.getElementById("lname");
 
+	form.addEventListener("submit",getData());
+
+	getData(){
+		Fn = fname.value;
+		ln = lname.value;
+		alert(fn,"",ln);
+	}
+	
 }
