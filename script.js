7015130da084b716b793d70da3c365a4d78da241
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-07 13:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-function getFormvalue() {
-	let form = document.getElementById("form1");
+
+var form = document.getElementById("form1");
     //Write your code here
 	let fname = document.getElementById("fname");
 	let lname = document.getElementById("lname");
 
-	form.addEventListener("submit",getData());
-
-	getData(){
-		Fn = fname.value;
-		ln = lname.value;
-		alert(fn,"",ln);
-	}
-	
-}
+	form.addEventListener("submit",function(e)
+    {
+        e.preventDefault();
+        console.log("fname",fname.value);
+        console.log(lname.value);
+    });
